5f689e8301b5ad4cb867791dfe0a67226baa26f3
Ziheng Zeng <zihengze@andrew.cmu.edu>
2024-01-29 16:53:43 -0500

    zizi added step5 in class exercise

---
+++ jan-29-paths/step51-29.py
@@ -0,0 +1,6 @@
+import os
+
+os.chdir("D:\Documents\Crafting Software sp 24\in-class-2024\jan-29-paths\musicians/rock")
+files = os.listdir ("D:\Documents\Crafting Software sp 24\in-class-2024\jan-29-paths\musicians/rock")
+
+print(files)
