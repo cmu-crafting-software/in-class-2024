a4993dfab65a4c674e1535a7bc30bf30b84188af
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2024-03-12 13:29:00 -0400

    feat: add 3/13 pbt lecture materials

---
+++ mar-13-pbt/wordle_parts_test.py
@@ -0,0 +1,10 @@
+from wordle_parts import is_five_letters, is_yellow, is_red, is_green
+from hypothesis import given, strategies as st
+
+@given(st.text())
+def test_is_five_letters_pbt(s: str) :
+    assert is_five_letters(s) == (len(s) == 5)
+
+@given(st.text(min_size=5, max_size=5),st.text(min_size=5, max_size=5), st.integers(min_value=0, max_value=4))
+def test_is_yellow_pbt(guess: str, secret: str, pos: int) :
+    pass # TODO: Replace with your code
