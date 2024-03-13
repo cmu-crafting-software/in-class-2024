fcbbb96da69ccd2862565f75f90e493ba993e796
Dailyn Despradel <95663713+dailyn269@users.noreply.github.com>
2024-03-13 20:56:02 +0000

    not finished method

--- mar-13-pbt/wordle_parts_test.py
+++ mar-13-pbt/wordle_parts_test.py
@@ -7,4 +7,17 @@ def test_is_five_letters_pbt(s: str) :
 
 @given(st.text(min_size=5, max_size=5),st.text(min_size=5, max_size=5), st.integers(min_value=0, max_value=4))
 def test_is_yellow_pbt(guess: str, secret: str, pos: int) :
-    pass # TODO: Replace with your code
+    if (is_green(guess, secret, pos)):
+        assert is_yellow(guess,secret, pos) == False 
+    elif (is_red(guess, secret, pos)):
+        assert is_yellow(guess,secret, pos) == False
+    else: 
+        assert is_yellow (guess, secret,pos) == True
+
+@given(st.text(min_size=5, max_size=5),st.text(min_size=5, max_size=5), st.integers(min_value=0, max_value=4))
+def test_pbt(guess: str, secret: str, pos: int) :
+    ## For a given letter alpha appearing m times in guess word exactly min (m,n) should be green or yellow where n is num tries aalpha appears in secret 
+    alpha = guess[pos]
+    n = secret.count(alpha)
+    m = guess.count(alpha)
+    assert min(m,n) == pos
