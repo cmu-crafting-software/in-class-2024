8ecb3f0c48f06cb7cccfd37f6eaebb642af239c3
Kara Ferner <76963214+karaferner@users.noreply.github.com>
2024-03-18 20:11:35 +0000

    pushing changes from last week

--- mar-11-pbt/wordle_parts_test.py
+++ mar-11-pbt/wordle_parts_test.py
@@ -1,7 +1,14 @@
 from wordle_parts import is_five_letters, is_yellow
+from hypothesis import given, strategies as st
+
 
 #def test_is_five_letters_true() :
 #    assert is_five_letters("tests")==True
 
 #def test_is_five_letters_false() :
 #    assert is_five_letters("test")==False
+
+
+@given(st.text())
+def test_is_five_letters_pbt(s: str):
+    assert is_five_letters(s) == (len(s) == 5)
