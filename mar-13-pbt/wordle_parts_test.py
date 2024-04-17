9afef8c84e93ccb55bbab48e7bdb470afaf0599e
Kara Ferner <76963214+karaferner@users.noreply.github.com>
2024-04-17 20:00:01 +0000

    commit to merge

--- mar-13-pbt/wordle_parts_test.py
+++ mar-13-pbt/wordle_parts_test.py
@@ -1,4 +1,4 @@
-from wordle_parts import is_five_letters, is_yellow, is_red, is_green
+from wordle_parts import * 
 from hypothesis import given, strategies as st
 
 @given(st.text())
@@ -7,32 +7,4 @@ def test_is_five_letters_pbt(s: str) :
 
 @given(st.text(min_size=5, max_size=5),st.text(min_size=5, max_size=5), st.integers(min_value=0, max_value=4))
 def test_is_yellow_pbt(guess: str, secret: str, pos: int) :
-    if  (is_green(guess, secret, pos)):
-        assert not is_yellow(guess,secret,pos)
-
-    elif (is_red(guess, secret, pos)):
-        assert not is_yellow(guess,secret,pos)
-
-    else:
-        assert is_yellow(guess,secret,pos)
-
-@given(st.text(min_size=5, max_size=5),st.text(min_size=5, max_size=5), st.integers(min_value=0, max_value=4))
-def test_num_green_yellow(guess: str, secret: str, pos: int) :
-    letter = guess[pos]
-
-    num_in_guess = 0
-    num_in_secret = 0
-
-    for n,m in zip(guess, secret):
-        if n == letter:
-            num_in_guess += 1
-        if m == letter:
-            num_in_secret += 1
-
-    count = 0
-    for index in range(5):
-        if (is_yellow(guess, secret, index) or is_green(guess, secret, index)):
-            count += 1
-
-    assert count == min(num_in_guess, num_in_secret)
-
+    pass # TODO: Replace with your code
